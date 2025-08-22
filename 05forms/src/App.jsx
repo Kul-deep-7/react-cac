62fe3690271c3d2d4124e2900d1a61ae9ca844d2
Kul-deep-7 <sahiljangid321@gmail.com>
2025-08-22 21:28:55 +0530

    basic form

---
+++ 05forms/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './App.css'
+
+function submission(e){
+  e.preventDefault() //first we prevent the default behaviour of form submission 
+  const f = e.target // then we get the form element using e.target
+  const form = new FormData(f) //formData is a built-in object that takes the form element as an argument and returns the form data as key-value pairs
+  const formdata = Object.fromEntries(form)// we convert the form data into an object using Object.fromEntries() method. (a methoda that that converts a list of key–value pairs into an object.)
+  console.log(formdata)
+
+}
+function App() {
+ 
+
+  return (
+    <>
+      <form onSubmit={submission}>
+        {/* Personal Info */}
+        <fieldset>
+          <legend>Personal Information</legend>
+
+          <label>Full Name:</label><br />
+          <input type="text" name="fullname" placeholder="Enter your name" required /><br /><br />
+
+          <label>Email:</label><br />
+          <input type="email" name="email" placeholder="Enter your email" required /><br /><br />
+
+          <label>Age:</label><br />
+          <input type="number" name="age" min="1" max="120" required /><br /><br />
+
+          <label>Gender:</label><br />
+          <input type="radio" name="gender" value="male" /> Male
+          <input type="radio" name="gender" value="female" /> Female
+          <input type="radio" name="gender" value="other" /> Other
+        </fieldset>
+        <br />
+
+
+        <input type="submit" value="Submit" />
+      </form>
+    </>
+  )
+}
+
+export default App
