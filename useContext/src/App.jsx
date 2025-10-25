3ef08d7614c8bea9070ba5796d6cbcccb37f404b
Kul-deep-7 <sahiljangid321@gmail.com>
2025-10-25 16:54:41 +0530

    contextapii

--- useContext/src/App.jsx
+++ useContext/src/App.jsx
@@ -21,3 +21,22 @@ function App() {
 }
 
 export default App
+
+/*
+first we create a Context object using createContext() in context.js file.
+Think of it like a “shared data channel” React can read/write from anywhere.
+
+Then, in App.jsx, we use CounterContext.Provider to wrap the part of our app that needs access to this shared data.
+Think of it like a “shared data channel” React can read/write from anywhere.(here: {count, increment}).
+Any component inside this provider tree can read those values.
+So Comp11 can now access count and increment through context.
+
+Then, in Comp11 we use useContext(CounterContext) to consume that shared data.
+const { count, increment } = useContext(CounterContext);
+This gives us direct access to count and increment without needing to pass them down as props.
+
+createContext() → sets up the shared data channel.
+Provider → makes the data available to all child components.
+useContext() → allows components to read from that shared data channel.
+This pattern helps avoid “prop drilling” where you’d have to pass props through many layers of components unnecessarily.
+*/
